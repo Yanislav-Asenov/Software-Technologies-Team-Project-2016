a9b9f7469f02613b59d18c3baba2359ef2d2d97f
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-08-15 00:01:44 +0300

    Added recent posts to home page and footer is always at the bottom of the page

--- SoftwareTechnologiesTeamProject/Controllers/HomeController.cs
+++ SoftwareTechnologiesTeamProject/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿namespace SoftwareTechnologiesTeamProject.Controllers
 {
+    using Models;
+    using System.Linq;
     using System.Web.Mvc;
+
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            var recentPosts = db.Posts.OrderByDescending(x => x.Date).Take(5).ToList();
+            ViewBag.RecentPosts = recentPosts;
+
             return View();
         }
 
