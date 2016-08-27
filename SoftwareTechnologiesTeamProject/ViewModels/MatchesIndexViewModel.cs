da23b4f0be794d4e726c47c392654553dc81069e
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-08-27 21:25:05 +0300

    Updated Fixtures View

--- SoftwareTechnologiesTeamProject/ViewModels/MatchesIndexViewModel.cs
+++ SoftwareTechnologiesTeamProject/ViewModels/MatchesIndexViewModel.cs
@@ -2,11 +2,33 @@
 {
     using Models;
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
 
     public class MatchesIndexViewModel
     {
         public List<Match> Matches { get; set; }
 
-        public ApplicationUser User { get; set; }
+        public Dictionary<string, List<Match>> GetUpcomingMatches()
+        {
+            var upcomingMatches = new Dictionary<string, List<Match>>();
+
+            foreach (var match in this.Matches.Where(m => m.IsResultUpdated == false).OrderBy(m => m.DateTime))
+            {
+                string key = match.DateTime.ToString("dddd dd MMMM yyyy", CultureInfo.InvariantCulture);
+
+                if (!upcomingMatches.ContainsKey(key))
+                {
+                    upcomingMatches.Add(key, new List<Match>());
+                    upcomingMatches[key].Add(match);
+                }
+                else
+                {
+                    upcomingMatches[key].Add(match);
+                }
+            }
+
+            return upcomingMatches;
+        }
     }
 }
